6cc3b10c4becf568513c3527e99bc6de3208178a
balqeesalkhatib <Balqees.alkhateb@gmail.com>
2024-05-03 20:10:29 +0300

    create role

--- backend/controllers/role.js
+++ backend/controllers/role.js
@@ -1,23 +1,22 @@
 const { pool } = require("../models/db");
 const createNewRole = (req, res) => {
-    const placeholder = req.body;
-    pool
-      .query(`INSERT INTO roles (role) VALUES($1)RETURNING *`, [placeholder.role])
-      .then((result) => {
-        res.status(201).json({
-          success: true,
-          massage: "Role created successfully",
-          role: result.rows,
-        });
-      })
-      .catch((error) => {
-        res.status(500).json({
-          success: false,
-          massage: "Server error",
-        });
+  const placeholder = req.body;
+  pool
+    .query(`INSERT INTO roles (role) VALUES($1)RETURNING *`, [placeholder.role])
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        massage: "Role created successfully",
+        role: result.rows,
       });
-  };
-  module.exports={
-    createNewRole,
-    
-  }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        massage: "Server error",
+      });
+    });
+};
+module.exports = {
+  createNewRole,
+};
