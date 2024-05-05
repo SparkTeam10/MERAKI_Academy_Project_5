6b6c45102cd5f527857bbe04d137634f84c83dc7
AdanAljboor <Adan.Aljboorr@Gmail.com>
2024-05-05 22:46:22 +0300

    added users <<register & registerServiceProvider & login

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 const roleRouter=require('./routes/role')
+const userRouter = require('./routes/users')
 // Handles any other endpoints [unassigned - endpoints]
 app.use("/roles",roleRouter)
+app.use("/users", userRouter)
+
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
 app.listen(PORT, () => {
--- backend/models/data.sql
+++ backend/models/data.sql
@@ -5,3 +5,42 @@ id SERIAL NOT NULL,
 role VARCHAR(255) NOT NULL,
 
 PRIMARY KEY (id) );
+
+-------------------------------
+
+CREATE TABLE users(
+
+id SERIAL PRIMARY KEY NOT NULL,
+
+userName VARCHAR(255) NOT NULL UNIQUE,
+
+age INT,
+
+phoneNumber INT,
+
+email VARCHAR(255) NOT NULL UNIQUE,
+
+password VARCHAR(255) NOT NULL,
+
+country VARCHAR(255),
+
+role_id INT,
+
+FOREIGN KEY (role_id) REFERENCES roles(id),
+
+is_deleted SMALLINT DEFAULT 0
+
+);
+
+-------------------------------
+
+CREATE TABLE categories(
+
+ id SERIAL PRIMARY KEY,
+ name VARCHAR(255),
+ description VARCHAR(255),
+ img TEXT,
+ is_delete SMALLINT DEFAULT 0 
+ 
+)
+
