f2e8a00bbeb0456e940bfe9bb64b5b6f854cf659
balqeesalkhatib <Balqees.alkhateb@gmail.com>
2024-05-06 16:02:05 +0300

    update,get all,delete for categories only for admin

--- backend/controllers/categories.js
+++ backend/controllers/categories.js
@@ -1,24 +1,102 @@
 const { pool } = require("../models/db");
 const createCategory = (req, res) => {
-    const {title,description,img}=req.body
-    pool.query(`
+  const { title, description, img } = req.body;
+  pool
+    .query(
+      `
     INSERT INTO categories(title,description,img) VALUES($1,$2,$3) RETURNING *
-    `,[title,description,img]) .then((result) => {
-        res.status(201).json({
-          success: true,
-          message: "product added successfully",
-          product: result.rows,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json({
-          success: false,
-          message: "an error occurred",
-          err,
-        });
+    `,
+      [title, description, img]
+    )
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: "product added successfully",
+        product: result.rows,
       });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({
+        success: false,
+        message: "an error occurred",
+        err,
+      });
+    });
+};
+const getAllCategories = (req, res) => {
+  pool
+    .query(
+      `
+SELECT * FROM categories WHERE is_deleted = 0
+`
+    )
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: `All the categories`,
+        result: result.rows,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server error`,
+        error: err.message,
+      });
+    });
+};
+const updateById = (req, res) => {
+  const { id } = req.params;
+  const { title, description, img } = req.body;
+  const values = [title || null, description || null, img || null, id];
+  const query = `UPDATE categories SET title=COALESCE($1,title),description=COALESCE($2,description),img=COALESCE($3,img) WHERE id=$4 RETURNING *;`;
+  pool
+    .query(query, values)
+    .then((result) => {
+      if (result.rows.length) {
+        return res.status(201).json({
+          success: true,
+          massage: "category Updated Successfully",
+          result: result.rows,
+        });
+      }
+      throw err;
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server error`,
+        error: err.message,
+      });
+    });
+};
+const deleteCategory = (req, res) => {
+  const { id } = req.params;
+  pool
+    .query(`UPDATE categories SET is_deleted = 1 WHERE id = $1 RETURNING *`, [id])
+    .then((result) => {
+      if (result.rows.length) {
+        return res.status(201).json({
+          success: true,
+          massage: "Product Updated Successfully",
+          result: result.rows,
+        });
+      }
+    //   throw err;
+    })
+    .catch((err) => {
+        console.log(err);
+      res.status(500).json({
+        success: false,
+        message: `Server error`,
+        error: err.message,
+      });
+    });
 };
 module.exports = {
   createCategory,
+  getAllCategories,
+  updateById,
+  deleteCategory,
 };
--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -3,8 +3,13 @@ const categoryRouter=express.Router();
 const authentication=require('../middleware/authentication')
 const authorization=require('../middleware/authorization')
 const {
-    createCategory
+    createCategory,
+    getAllCategories,
+    updateById,
+    deleteCategory
 }=require('../controllers/categories');
 categoryRouter.post('/',authentication,authorization("create_category"),createCategory)
-
+categoryRouter.get('/',getAllCategories)
+categoryRouter.put('/:id',authentication,authorization("update_category"),updateById)
+categoryRouter.delete('/:id',authentication,authorization("delete_category"),deleteCategory)
 module.exports=categoryRouter;
