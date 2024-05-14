e92dce3e6a3a34efe90b47acfb09423e532626cb
AdanAljboor <Adan.Aljboorr@Gmail.com>
2024-05-14 21:04:10 +0300

    loginFront

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -4,11 +4,13 @@ const Navbar = () => {
   return (
    <nav>
     <h1>Spark</h1>
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="about">About</NavLink>
-    <NavLink to="help">Help</NavLink>
-    <NavLink to="category">Category</NavLink>
-    <NavLink to ="register">register</NavLink>
+    <NavLink to="/">Home </NavLink>
+    <NavLink to="about"> About </NavLink>
+    <NavLink to="help"> Help </NavLink>
+    <NavLink to="category"> Category </NavLink>
+    <NavLink to ="register"> register </NavLink>
+    <NavLink to = "login"> Login </NavLink>
+    <NavLink to = "Booking">  Booking </NavLink>
 
    </nav>
   )
