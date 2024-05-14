bf12df4e3b525dc6bc931299d2995e03fd9e3b2a
balqeesalkhatib <Balqees.alkhateb@gmail.com>
2024-05-14 23:31:01 +0300

    conflect

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,30 +1,20 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
-   <nav>
-    <h1>Spark</h1>
+    <nav>
+      <h1>Spark</h1>
 
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="about">About</NavLink>
-    <NavLink to="help">Help</NavLink>
-    <NavLink to="category">Category</NavLink>
-    <NavLink to ="register">register</NavLink>
-    <NavLink to ="/allserviceprovider">All-Service</NavLink>
-    
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="about">About</NavLink>
+      <NavLink to="help">Help</NavLink>
+      <NavLink to="category">Category</NavLink>
+      <NavLink to="register">register</NavLink>
+      <NavLink to="/allserviceprovider">All-Service</NavLink>
+      <NavLink to="login"> Login </NavLink>
+      <NavLink to="Booking"> Booking </NavLink>
+    </nav>
+  );
+};
 
-    <NavLink to="/">Home </NavLink>
-    <NavLink to="about"> About </NavLink>
-    <NavLink to="help"> Help </NavLink>
-    <NavLink to="category"> Category </NavLink>
-    <NavLink to ="register"> register </NavLink>
-    <NavLink to = "login"> Login </NavLink>
-    <NavLink to = "Booking">  Booking </NavLink>
-
-
-   </nav>
-  )
-}
-
-export default Navbar
-
+export default Navbar;
