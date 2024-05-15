841a6b682911e1b04045dbb725c9ec7d9e463f88
AllaRabeaa <allaa.emad.rabei@gmail.com>
2024-05-15 11:28:17 +0300

    create button delete

--- frontend/src/pages/AllService/index.js
+++ frontend/src/pages/AllService/index.js
@@ -1,64 +1,64 @@
-import React, { Suspense, useState,useEffect } from "react";
+import React, { Suspense, useState, useEffect } from "react";
 import axios from "axios";
 import { useLoaderData, Await, useNavigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import { useDispatch, useSelector } from "react-redux";
 
-import { setDeleteService ,setAllService} from '../../Service/redux/reducers/serviceprovider'
+import { setDeleteService, setAllService } from '../../Service/redux/reducers/serviceprovider'
 
 export default function GetAllService() {
-const dispatch = useDispatch()
-const navigate = useNavigate();
-const {service} =useSelector((state)=>{
-    return {
-        service:state.service.service
+    const dispatch = useDispatch()
+    const navigate = useNavigate();
+    const { service } = useSelector((state) => {
+        return {
+            service: state.service.service
+        }
+    })
+    const allSerivces = async () => {
+        try {
+            const results = await axios.get(`http://localhost:5001/serviceProvider/`);
+            console.log(results)
+            dispatch(setAllService(results.data.result))
+        } catch (err) {
+            console.log(err)
+        }
     }
-})
-const allSerivces = async()=>{
-try{
-    const results= await axios.get(`http://localhost:5001/serviceProvider/`);
-    console.log(results)
-    dispatch(setAllService(results.data.result))
-}catch(err){
-console.log(err)
-}
-}
-useEffect(() => {
-   
-    allSerivces()
-}, [])
+    useEffect(() => {
 
-   
-    
+        allSerivces()
+    }, [])
+
+
+
 
     return (
         <div>
             <h1>All Service</h1>
-                            <div>
-                                {service&&
-                                    
-                                    service.map((elem, i) => {
-                                       
-                                    return <div key={i}>
-                                        <h3>{elem.title}</h3>
-                                        <p>{elem.description}</p>
-                                        <h2>{elem.address}</h2>
-                                        <img src={elem.img} />
-                                        <h1>{elem.price}</h1>
-                                        <button onClick={()=>{
-                                            
-                                            dispatch(setDeleteService(elem.id))
-                                        }}>Delet</button>
-                                    </div>
-                                })}
-                            
-                            </div>
+            <div>
+                {service &&
+
+                    service.map((elem, i) => {
+
+                        return <div key={i}>
+                            <h3>{elem.title}</h3>
+                            <p>{elem.description}</p>
+                            <h2>{elem.address}</h2>
+                            <img src={elem.img} />
+                            <h1>{elem.price}</h1>
+                            <button onClick={() => {
+                                axios.delete(`http://localhost:5001/serviceProvider/${elem.id}`)
+                                dispatch(setDeleteService(elem.id))
+                            }}>Delet</button>
+                        </div>
+                    })}
+
+            </div>
             <br></br>
             <button onClick={() => {
                 navigate('/createprovider')
             }}>
-            Create Provider</button>
-            
+                Create Provider</button>
+
             <button onClick={() => {
                 navigate('/')
             }}>
