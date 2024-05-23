202ac433da27e8e6b99f3a74d1d4d24a66eeacee
AdanAljboor <Adan.Aljboorr@Gmail.com>
2024-05-23 21:27:04 +0300

    Save changes before switching branches

--- frontend/src/components/OAuth/index.js
+++ frontend/src/components/OAuth/index.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import {GoogleAuthProvider, signInWithPopup , getAuth} from "firebase/auth"
-import { app } from '../firebase'
+// import React from 'react'
+// import {GoogleAuthProvider, signInWithPopup , getAuth} from "firebase/auth"
+// import { app } from '../firebase'
 
 
-const OAuth = () => {
+// const OAuth = () => {
 
-    const handleGoogleClick = async()=>{
-      try{
-        const Provider = new GoogleAuthProvider()
-        const auth = getAuth(app)
-        const result = await signInWithPopup(auth , Provider)
-        console.log(result);
-      } catch(error){
-        console.log("could not login with google " , error.message);
-      }
-    }
-  return (
-    <div>
+//     const handleGoogleClick = async()=>{
+//       try{
+//         const Provider = new GoogleAuthProvider()
+//         const auth = getAuth(app)
+//         const result = await signInWithPopup(auth , Provider)
+//         console.log(result);
+//       } catch(error){
+//         console.log("could not login with google " , error.message);
+//       }
+//     }
+//   return (
+//     <div>
        
-       <button type='button' onClick={handleGoogleClick}>
-        Continue with google</button> 
+//        <button type='button' onClick={handleGoogleClick}>
+//         Continue with google</button> 
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default OAuth
+// export default OAuth
